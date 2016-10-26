b8287674118e3623748a89787f84627c92c16548
Thien Pham <ThienPham@ishcmc.edu.vn>
2016-10-26 15:12:23 +0700

    angular2

---
+++ source/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/template/site.tpl.html'
+})
+export class AppComponent { }
---
+++ source/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+  selector:'my-header',
+  template:'<div> This is header working !!</div>'
+
+})
+
+export class HeaderComponent{}
